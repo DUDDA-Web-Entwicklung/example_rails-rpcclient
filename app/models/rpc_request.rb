940ff9669c6a96101ee8412b7164d3d00c4c9fd2
Manuel Dudda <manueldudda@Trooper-Dt-3.local>
2012-10-30 14:26:18 +0100

    createOptions

--- app/models/rpc_request.rb
+++ app/models/rpc_request.rb
@@ -1,3 +1,11 @@
 class RpcRequest < ActiveRecord::Base
   attr_accessible :methodName, :params, :option_id
+
+  def option_id
+  	@option_id
+  end
+
+  def option_id=(arg)
+  	@option_id = arg
+  end
 end
